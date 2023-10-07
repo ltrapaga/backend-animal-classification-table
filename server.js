0a6209bf29a7697ff55af48bfccbc61a0b9b0782
Laura Trapaga <trapaga9@gmail.com>
2023-10-06 21:17:05 -0400

    Install MySQL     Create `basic_backend_table` database     Connect to database     Start server

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
 const express = require('express');
+const mysql = require('mysql');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.statusCode = 200
-    res.setHeader('Conent-Type', 'text-plain')
-    res.send('Hello World')
-})
+// Create connection to the database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'SswUfu*Esuub3!.',
+    database: 'basic_backend_table'
+});
 
-app.listen(port, () => {
-    console.log(`Server listening on port ${port}`)
+// Open the MYSQL connection
+connection.connect(error => {
+    if (error) {
+        console.log('Error connecting to database.');
+        throw error;
+    }
+    app.get('/', (req, res) => {
+        res.statusCode = 200
+        res.setHeader('Conent-Type', 'text-plain')
+        res.send('Hello World')
+    })
+    // Start the Express server
+    app.listen(port, () => {
+        console.log(`Server listening on port ${port}`)
+    })
 })
