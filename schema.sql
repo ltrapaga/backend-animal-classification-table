a73b03e8fa711b83b7f22979c8d5da4f46a59a57
Laura Trapaga <trapaga9@gmail.com>
2023-10-09 16:17:04 -0400

    Inserted data into `animal_families` table

--- schema.sql
+++ schema.sql
@@ -4,13 +4,22 @@ CREATE DATABASE basic_backend_table;
 -- Use `basic_backend_table` database
 USE basic_backend_table;
 
--- Creates `animal_families table --
+-- Creates `animal_families` table --
 CREATE TABLE IF NOT EXISTS animal_families (
 -- Creates numeric field (or column) called `id`
 id INT NOT NULL,
 -- Creates a string called "name" with 20 or less characters --
-name VARCHAR(20) NOT NULL,
+family_name VARCHAR(20) NOT NULL,
 -- Adds the primary key constraint on the column `id` --
 PRIMARY KEY (id)
 );
 
+-- Insert values into the `id` and `family_name` records from the `animal_families` table --
+INSERT INTO animal_families (id, family_name) VALUES
+(1, 'Felidae'),
+(2, 'Ursidae'),
+(3, 'Canidae'),
+(4, 'Mustelidae');
+
+-- Fetch all fields from `animal_families` to verify data was inserted --
+SELECT * FROM animal_families;
